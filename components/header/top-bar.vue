4e0c2436ce990eed1f0593d2ca558a06757ad3a4
w.shawn <1124170486@qq.com>
2020-01-04 12:25:59 +0800

    新增搜索相关内容

---
+++ components/header/top-bar.vue
@@ -0,0 +1,28 @@
+<template>
+  <el-row class="m-header" :gutter="0">
+    <el-col :span="5">
+      <geo></geo>
+    </el-col>
+    <el-col :span="5">
+      <user></user>
+    </el-col>
+    <el-col :span="14">
+      <nav-bar></nav-bar>
+    </el-col>
+  </el-row>
+</template>
+<script>
+import Geo from "./geo.vue";
+import User from "./user.vue";
+import NavBar from "./nav.vue";
+export default {
+  components: {
+    Geo,
+    User,
+    NavBar
+  }
+};
+</script>
+<style lang="scss">
+@import "@/assets/css/public/layout.scss";
+</style>
